497963deece52b3c5425e7ceede3d1977f7fdd81
abyan28 <v28mtyqm.mu1@gmail.com>
2017-12-05 23:19:42 +0700

    2

--- cp.c
+++ cp.c
@@ -3,3 +3,14 @@
 #include "user.h"
 #include "fcntl.h"
 
+int main(int argc, char *argv[])
+{
+	int fd, dfd, r, w;
+	char *src, *dest;
+	char buf[9999];
+	
+	if(argc <= 2)
+	{
+		printf(1, "Cannot copy its self!\n");
+		exit();
+	}
