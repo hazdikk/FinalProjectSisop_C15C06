b582207be2c69e296b9d9366bb18b906eeb4bc3a
abyan28 <v28mtyqm.mu1@gmail.com>
2017-12-05 23:20:33 +0700

    3

--- cp.c
+++ cp.c
@@ -14,3 +14,18 @@ int main(int argc, char *argv[])
 		printf(1, "Cannot copy its self!\n");
 		exit();
 	}
+
+	src = argv[1];
+	dest = argv[2];
+
+	if((fd = open(src, O_RDONLY)) < 0) 
+	{
+		printf(2, "cp: cannot open source %s\n", src);
+		exit();
+	}
+
+	if((dfd = open(dest, O_CREATE|O_WRONLY)) < 0)
+	{
+		printf(2, "cp: cannot open destination %s\n", dest);
+		exit();
+	}
