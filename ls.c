69ea6425271db869c9bb6e0d46cd8525a35452c0
HIlmi Raditya <raditya113@icloud.com>
2017-12-07 00:12:35 +0700

    ls
    kelar

--- ls.c
+++ ls.c
@@ -1,54 +1,85 @@
-#include <types.h>
-#include <stat.h>
-#include <user.h>
-#include <fs.h>
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fs.h"
 
+char* fmtname(char *path)
+{
+  static char buf[DIRSIZ+1];
+  char *p;
+  
+  // Find first character after last slash.
+  for(p= path+strlen(path) ; p >= path && *p != '/'; p--)
+    ;
+  p++;
+  
+  // Return blank-padded name.
+  if(strlen(p) >= DIRSIZ)
+    return p;
+  memmove(buf, p, strlen(p));
+  memset(buf+strlen(p), ' ', DIRSIZ-strlen(p));
+  return buf;
+}
 
 void ls(char *path)
 {
-	char *p;
-	char buf[512]; //
-	struct dirent de; // fs.h  
-	struct stat st; //stat.h
-	int file_open//untuk mendapatkan hasil dari open(path,0)
-
-	if((file_open=open(path,0)) < 0)
-	{
-		printf(2,"ls : tidak dapat dibuka %s\n", path);
-		return;
-	}
-
-	if(fstat(file_open, &st))
-	{
-		printf(2,"ls:tidak dapat stat %s\n", path);
-		close(file_open)
-		return;
-	}
-
-	switch (st.type)
-	{
-		case T_FILE:
-			printf(1,"%s %d %d %d\n", fmtname(path), st.type, st.ino, st.size);
-
-		case T_DIR:
-			if(strlen(path) + 1 + DIRSIZ + 1) > sizeof buf
-			{
-      			printf(1, "ls: path too long\n");
-      			break;
-			}
-			
-	}
-
+  char buf[512], *p;
+  int fd;
+  struct dirent de;
+  struct stat st;
+  
+  if((fd = open(path,0)) < 0){
+    printf(2, "ls: cannot open %s\n", path);
+    return;
+  }
+  
+  if(fstat(fd, &st) < 0){
+    printf(2, "ls: cannot stat %s\n", path);
+    close(fd);
+    return;
+  }
+  
+  switch(st.type)
+  {
+  case T_FILE:
+    printf(1, "%s %d %d %d\n", fmtname(path), st.type, st.ino, st.size);
+    break;
+  
+  case T_DIR:
+    if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf)
+    {
+      printf(1, "ls: path too long\n");
+      break;
+    }
+    strcpy(buf, path);
+    p = buf+strlen(buf);
+    *p++ = '/';
+    while(read(fd, &de, sizeof(de)) == sizeof(de))
+    {
+      if(de.inum == 0) continue;
+      memmove(p, de.name, DIRSIZ);
+      p[DIRSIZ] = 0;
+      if(stat(buf, &st) < 0)
+      {
+        printf(1, "ls: cannot stat %s\n", buf);
+        continue;
+      }
+      printf(1, "%s %d %d %d\n", fmtname(buf), st.type, st.ino, st.size);
+    }
+    break;
+  }
+  close(fd);
 }
 
 int main(int argc, char *argv[])
 {
-	if (argc <2)
-	{
-		//masuk ke fungsi ls
-	}
-	for (int a=1;a<argc;a++)
-	{
-		//masuk ke fungsi ls berdasarkan format input yang dimasukkan
-	}
+  int i;
+
+  if(argc < 2){
+    ls(".");
+    exit();
+  }
+  for(i=1; i<argc; i++)
+    ls(argv[i]);
+  exit();
 }
