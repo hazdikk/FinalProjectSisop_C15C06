f6026bf1d8de2acefd3df1ebc8fb5d23b54633c5
HIlmi Raditya <raditya113@icloud.com>
2017-12-06 21:39:39 +0700

    ls
    penambahan case apabila file tidak dapat dibuka

--- ls.c
+++ ls.c
@@ -3,6 +3,30 @@
 #include <user.h>
 #include <fs.h>
 
+
+void ls(char *path)
+{
+	char *p;
+	char buf[512]; //
+	struct dirent de; // fs.h  
+	struct stat st; //stat.h
+	int file_open//untuk mendapatkan hasil dari open(path,0)
+
+	if((file_open=open(path,0)) < 0)
+	{
+		printf(2,"ls : tidak dapat dibuka %s\n", path);
+		return;
+	}
+
+	if(fstat(file_open, &st))
+	{
+		printf(2,"ls:tidak dapat stat %s\n", path);
+		close(file_open)
+		return;
+	}
+
+}
+
 int main(int argc, char *argv[])
 {
 	if (argc <2)
