32f705c2326e29633fdd9cada849d8078d865274
HIlmi Raditya <raditya113@icloud.com>
2017-12-07 00:19:04 +0700

    Touch
    Sip Mantap

--- touch.c
+++ touch.c
@@ -1,2 +1,15 @@
 #include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
 
+int main (int argc, char* argv[])
+{
+	if (argc < 2) printf(1,"Kesalahan syntax, seharusnya touch NAMAFILE");
+
+	int a, file;
+	for (a=1;a<argc;a++)
+	{
+		file = open(argv[a], 0_CREATE);
+	}
+}
