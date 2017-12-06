98625446d2db212ef8b93777aca2597d0e2e383b
HIlmi Raditya <raditya113@icloud.com>
2017-12-06 21:11:15 +0700

    Bismillah FP Lancar
    Penambahan pada int main()

--- ls.c
+++ ls.c
@@ -1,8 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <types.h>
+#include <stat.h>
+#include <user.h>
+#include <fs.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-	printf("Hello World !\n");
-	return 0;
+	if (argc <2)
+	{
+		//masuk ke fungsi ls
+	}
+	for (int a=1;a<argc;a++)
+	{
+		//masuk ke fungsi ls berdasarkan format input yang dimasukkan
+	}
 }
