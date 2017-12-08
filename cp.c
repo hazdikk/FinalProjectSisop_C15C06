c29b4ea935bdda73b2741c5794713c6727e40210
abyan28 <32873494+abyan28@users.noreply.github.com>
2017-12-08 19:43:04 +0700

    5

--- cp.c
+++ cp.c
@@ -3,6 +3,7 @@
 #include "user.h"
 #include "fcntl.h"
 
+
 int main(int argc, char *argv[])
 {
 	int fd, dfd, r, w;
@@ -11,7 +12,7 @@ int main(int argc, char *argv[])
 	
 	if(argc <= 2)
 	{
-		printf(1, "Cannot copy its self!\n");
+		printf(1, "Please input src and dest\n");
 		exit();
 	}
 
